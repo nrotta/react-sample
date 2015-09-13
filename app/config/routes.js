fc326fdaacc16f0d16a0190d1f1decd92c68c851
Nicolas Rotta <nicolasrotta@gmail.com>
2015-09-13 11:38:05 +0100

    Initial commit

---
+++ app/config/routes.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var Router = require('react-router');
+
+var Main = require('../components/Main');
+var Home = require('../components/Home');
+var Profile = require('../components/Profile');
+
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+
+module.exports = (
+  <Route name="app" path="/" handler={Main}>
+    <Route name="profile" path="/profile/:username" handler={Profile} />
+    <DefaultRoute handler={Home} />
+  </Route>
+);
